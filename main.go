be0c668b36c5537cf2e70b5489599a89a31ffff4
Chyroc <chen_yunpeng@foxmail.com>
2018-05-23 19:54:47 +0800

    add http router

--- main.go
+++ main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
+
 	"github.com/Chyroc/leetcode-badge/internal"
 )
 
 func main() {
-	data, err := internal.FetchLeetcodeData("todo")
-	if err != nil {
-		panic(err)
-	}
+	app := gin.Default()
 
-	fmt.Printf("%#v\n", data)
+	app.GET("/", func(c *gin.Context) {
+		name := c.Query("name")
+		if name == "" {
+			c.JSON(400, map[string]interface{}{"message": "name is empty"})
+			return
+		}
+
+		data, err := internal.FetchLeetcodeData(name)
+		if err != nil {
+			c.JSON(400, map[string]interface{}{"message": err})
+			return
+		} else if data == nil || len(data) == 0 {
+			c.JSON(400, map[string]interface{}{"message": fmt.Sprintf("fetch leetcode data of account: %s, result: nil", name)})
+			return
+		}
+
+		c.JSON(200, data)
+		return
+	})
+
+	app.Run(":9090")
 }
